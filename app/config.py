3e9f437b0436bd530b9d7bb1e43f5caad0b65e58
Ongdik <ehddlr27@gmail.com>
2025-11-06 11:53:26 +0900

    Refactor(config.py): 출력 로그를 로거 기반으로 변경

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from app.utils.logger_config import setup_logger
 
 load_dotenv()
 
@@ -26,18 +27,26 @@ class Config:
     # 카메라 연결 오류로 인해 테스트용 코드
     USE_MOCK_CAMERA = os.getenv("USE_MOCK_CAMERA", "false").lower() == "true"
     
+    # 로거 초기화
+    logger = setup_logger(__name__)
+
     @staticmethod
     def init_app():
         """애플리케이션 시작 시 필요한 초기화"""
-        print(f"DEBUG: SAVE_DIR = {Config.SAVE_DIR}")
-        print(f"DEBUG: AI_SERVER_URL = {Config.AI_SERVER_URL}")
-        print(f"DEBUG: HOST = {Config.HOST}")
-        print(f"DEBUG: PORT = {Config.PORT}")
-        print(f"DEBUG: USE_MOCK_CAMERA = {Config.USE_MOCK_CAMERA}")
+        Config.logger.info("=== Config 초기화 ===")
+        Config.logger.info(f"SAVE_DIR = {Config.SAVE_DIR}")
+        Config.logger.info(f"AI_SERVER_URL = {Config.AI_SERVER_URL}")
+        Config.logger.info(f"FRONTEND_URL = {Config.FRONTEND_URL}")
+        Config.logger.info(f"HOST = {Config.HOST}")
+        Config.logger.info(f"PORT = {Config.PORT}")
+        Config.logger.info(f"USE_MOCK_CAMERA = {Config.USE_MOCK_CAMERA}")
         os.makedirs(Config.SAVE_DIR, exist_ok=True)
         
         if not Config.AI_SERVER_URL:
-            raise ValueError("AI_SERVER_URL이 .env 파일에 설정되어 있지 않습니다!")
+            Config.logger.error("AI_SERVER_URL이 .env 파일에 설정되어 있지 않습니다.")
+            raise ValueError("AI_SERVER_URL이 .env 파일에 설정되어 있지 않습니다.")
+
+        Config.logger.info("Config 초기화 성공.\n")
     
     @staticmethod
     def get_summary():
